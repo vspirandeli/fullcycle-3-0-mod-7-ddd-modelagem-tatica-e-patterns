0e0f3e6fce367758df9a3e352e9a397d45689102
Victor Spirandeli <vspirandeli@gmail.com>
2024-08-25 22:43:20 -0300

    aula: consistencia constante em primeiro lugar

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,13 +1,42 @@
+// class Customer {
+//   _id: string;
+//   _name: string;  
+//   _address: string;
+//   _active: boolean = true;
+
+//   constructor(id: string, name: string, address: string) {
+//     this._id = id;
+//     this._name = name;
+//     this._address = address;
+//   }
+
+//   // Trás uma regra de negócio para a entidade. Mais semântica.
+//   changeName(name: string) {
+//     this._name = name;
+//   }
+
+//   activate() {
+//     this._active = true;
+//   }
+
+//   deactivate() {
+//     this._active = false;
+//   }
+// }
+
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+// Nesse ponto do código é mostrado a inconsistência dos dados. Pois não existe cliente sem nome. 
+// A entdade deve estar consistente 100% do tempo.
 class Customer {
   _id: string;
-  _name: string;  
-  _address: string;
+  _name: string = "";  
+  _address: string = "";
   _active: boolean = true;
 
-  constructor(id: string, name: string, address: string) {
+  constructor(id: string) {
     this._id = id;
-    this._name = name;
-    this._address = address;
   }
 
   // Trás uma regra de negócio para a entidade. Mais semântica.
@@ -24,7 +53,12 @@ class Customer {
   }
 }
 
+let cliente = new Customer("1");
+cliente.changeName("João");
 
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////
 // Exemplo de entidade anêmica. A entidade não possui comportamento, apenas atributos.
 // A entidade é responsável por armazenar os dados de um cliente. Sem regras de negócio.
 // class Customer {
