8f33bebb21471395d8dc16e2d8f97abfc76dc393
Victor Spirandeli <vspirandeli@gmail.com>
2024-08-28 23:28:25 -0300

    aula: ajuste price do OrderItem

--- src/entity/order_item.ts
+++ src/entity/order_item.ts
@@ -1,15 +1,21 @@
 export default class OrderItem {
-  _id: string;
-  _name: string;
-  _price: number;
+  private _id: string;
+  private _name: string;
+  private _price: number;
+  private _quantity: number;
 
-  constructor(id: string, name: string, price: number) {
+  constructor(id: string, name: string, price: number, quantity: number) {
     this._id = id;
     this._name = name;
     this._price = price;
+    this._quantity = quantity;
   }
 
   get price(): number {
     return this._price;
+  }
+
+  orderItemTotal(): number {
+    return this._price * this._quantity;
   }
 }
