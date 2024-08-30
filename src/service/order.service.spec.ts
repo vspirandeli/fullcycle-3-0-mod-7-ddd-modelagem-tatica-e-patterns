5c5d27faa398764afd6034c067d7bf02d3d4e722
Victor Spirandeli <vspirandeli@gmail.com>
2024-08-30 20:27:58 -0300

    aula: realizando cálculo com order service

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,5 +1,6 @@
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item"
+import OrderService from "./order.service";
 
 describe('Service unit tests', () => {
   it('should get total of all orders', () => {
